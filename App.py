225d850191999d5f3ec40649388d83e977fc58e5
Ashutosh Jha <129574087+Ashutxshj@users.noreply.github.com>
2024-02-25 20:08:15 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import difflib
-from nltk.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 
